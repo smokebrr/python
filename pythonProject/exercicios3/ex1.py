a30f1a15d64eed78a90407f3d528f1a57100066c
calouros <calouros@fatec.corp>
2025-03-27 12:06:42 -0300

    ex3

---
+++ pythonProject/exercicios3/ex1.py
@@ -0,0 +1,15 @@
+# 1. Conversor de Moedas
+#Implemente um conversor de moedas que permita ao usuário escolher entre Dólar (USD), Euro (EUR) e Libra (GBP) e converter um valor informado para Reais (BRL).
+
+opcao = str(input("Digite uma das opcoes para realizar a conversao \n USD \n EUR \n GBP"))
+valor = int(input("Digite um valor para conversao em BRL"))
+
+match opcao:
+    case USD:
+        print(f"USD: {valor*5.7}")
+    case EUR:
+        print(f"EUR: {valor*6.1}")
+    case GBP:
+        print(f"GBP: {valor*7.4}")
+    case _:
+        print(f"opcao invalida")
