8703510eb3a9cbd46c78a1d334c6a4c721ce1a8b
smokebrr <igormecmec20@gmail.com>
2025-05-28 11:45:12 -0300

    att

---
+++ pythonProject/exercicios6/ex1.py
@@ -0,0 +1,5 @@
+# Crie uma lista com 5 números inteiros e exiba cada número ao quadrado.
+
+lista = [1,2,3,4,5]
+for i in range(5):
+    print(i**2)
---
+++ pythonProject/exercicios6/ex2.py
@@ -0,0 +1,7 @@
+#Peça ao usuário para digitar 5 nomes e armazene-os em uma lista. Depois, exiba os nomes um por um.
+nomes = []
+for i in range(5):
+    nomes = str(input("Digite 5 nomes: "))
+for i in range(5):
+    print([nomes])
+    
